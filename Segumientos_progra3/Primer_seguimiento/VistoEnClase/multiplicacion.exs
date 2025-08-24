fae1e9ac5ed63a8708d99c201877e786962cc8d4
JuanDCT99 <juand.cardozot@uqvirtual.edu.co>
2025-08-23 19:05:29 -0500

    Add files via upload
    Códigos trabajados en clase

---
+++ Segumientos_progra3/Primer_seguimiento/VistoEnClase/multiplicacion.exs
@@ -0,0 +1,23 @@
+a = 12
+b = 12
+c = 12
+
+suma = a + b + c
+resta = a - b - c
+producto = a * b * c
+division = a / b / c
+potencia = a ** b ** c
+
+
+IO.puts("Aqui se realizara una serie de operaciones")
+IO.puts("La suma es = #{suma}")
+IO.puts("La resta es = #{resta}")
+IO.puts("La producto es = #{producto}")
+IO.puts("La division es = #{division}")
+IO.puts("La potencia es = #{potencia}")
+
+"Holas"
+|>IO.puts()
+
+"Hola a todos, soy estudiante de la Uniquindio"
+|> IO.puts()
